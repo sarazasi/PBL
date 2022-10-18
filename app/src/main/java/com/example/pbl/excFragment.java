4baadc109d8d71b7d892643bd48c74d4be18bfdf
sarazasi <yamadatakumin@icloud.com>
2022-10-18 18:11:09 +0900

    first commit

---
+++ app/src/main/java/com/example/pbl/excFragment.java
@@ -0,0 +1,28 @@
+package com.example.pbl;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class excFragment extends Fragment {
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    //レイアウトファイルを取得
+    View view = inflater.inflate(R.layout.fragment_exc, container, false);
+    //clickイベントリスナーを登録(運動)
+    view.findViewById(R.id.btnHome).setOnClickListener(
+            new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.homeFragment);
+              }
+            }
+    );
+    return view;
+  }
+}
---
+++ app/src/main/java/com/example/pbl/homeFragment.java
@@ -0,0 +1,39 @@
+package com.example.pbl;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class homeFragment extends Fragment {
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    //レイアウトファイルを取得
+    View view = inflater.inflate(R.layout.fragment_home, container, false);
+    //clickイベントリスナーを登録(運動)
+    view.findViewById(R.id.btnExc).setOnClickListener(
+            new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.excFragment);
+              }
+            }
+    );
+
+    //clickイベントリスナーを登録(ウォーキング)
+    view.findViewById(R.id.btnWalk).setOnClickListener(
+            new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.walkFragment);
+              }
+            }
+    );
+
+    return view;
+  }
+}
