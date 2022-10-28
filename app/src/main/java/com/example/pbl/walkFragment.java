41ffa17d14c78cbe13d8a0569c2d533629880dec
te19yamauchi <te19yamauchi@kumamoto.kosen-nct.ac.jp>
2022-10-28 10:42:33 +0900

    Mapの追加(非検証)

--- app/src/main/java/com/example/pbl/walkFragment.java
+++ app/src/main/java/com/example/pbl/walkFragment.java
@@ -1,6 +1,13 @@
 package com.example.pbl;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
@@ -10,10 +17,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class walkFragment extends Fragment {
+  private MapView mMapView;
+
+  private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+  @SuppressLint("MissingInflatedId")
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     //レイアウトファイルを取得
     View view = inflater.inflate(R.layout.fragment_walk, container, false);
+    Bundle mapViewBundle = null;
+    if (savedInstanceState != null) {
+      mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+    }
+    mMapView = (MapView) view.findViewById(R.id.mapView);
+    mMapView.onCreate(mapViewBundle);
+
+    mMapView.getMapAsync((OnMapReadyCallback) this);
     //clickイベントリスナーを登録(運動)
     view.findViewById(R.id.btnHome2).setOnClickListener(
             new View.OnClickListener() {
@@ -24,5 +43,58 @@ public class walkFragment extends Fragment {
             }
     );
     return view;
+  }
+  @Override
+  public void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+
+    Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+    if (mapViewBundle == null) {
+      mapViewBundle = new Bundle();
+      outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+    }
+
+    mMapView.onSaveInstanceState(mapViewBundle);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    mMapView.onResume();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    mMapView.onStart();
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    mMapView.onStop();
+  }
+
+  @Override
+  public void onMapReady(GoogleMap map) {
+    map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+  }
+
+  @Override
+  protected void onPause() {
+    mMapView.onPause();
+    super.onPause();
+  }
+
+  @Override
+  protected void onDestroy() {
+    mMapView.onDestroy();
+    super.onDestroy();
+  }
+
+  @Override
+  public void onLowMemory() {
+    super.onLowMemory();
+    mMapView.onLowMemory();
   }
 }
