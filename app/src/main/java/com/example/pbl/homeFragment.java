8c79227e636a7f27d1dfdb366a249297c560b127
te19jige <te19jige@g.kumamoto-nct.ac.jp>
2022-11-16 15:04:14 +0900

    体操のページの変更

--- app/src/main/java/com/example/pbl/homeFragment.java
+++ app/src/main/java/com/example/pbl/homeFragment.java
@@ -12,9 +12,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.Objects;
 
 public class homeFragment extends Fragment {
   @SuppressLint("SetTextI18n")
@@ -42,9 +39,12 @@ public class homeFragment extends Fragment {
             }
     );
 
+
+
     TextView txt = view.findViewById(R.id.txtValue);
     Bundle args = requireArguments();
-    txt.setText("乱数" + homeFragmentArgs.fromBundle(args).getNum());
+    txt.setText(" " + homeFragmentArgs.fromBundle(args).getNum());
+
 
     //カレンダー textView
     CalendarView cal;
@@ -54,7 +54,7 @@ public class homeFragment extends Fragment {
               @Override
               public void onSelectedDayChange(@NonNull CalendarView v, int year, int month, int dayOfMonth) {
                 //カレンダー textView
-                TextView txt = view.findViewById(R.id.textView);
+                TextView txt = view.findViewById(R.id.textbtn1);
                 txt.setText(dayOfMonth + "日の運動量");
               }
             }
