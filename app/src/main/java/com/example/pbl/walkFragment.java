d2e988007b3827f3d47877a87ab4ff3d4fc0919d
Matchaice <te19yamauchi@g.kumamoto-nct.ac.jp>
2022-12-13 07:17:11 +0900

    ウォーキング開始及び終了ボタンの設置、移動距離記録機能の追加

--- app/src/main/java/com/example/pbl/walkFragment.java
+++ app/src/main/java/com/example/pbl/walkFragment.java
@@ -1,15 +1,18 @@
 package com.example.pbl;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
@@ -21,7 +24,10 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class walkFragment extends Fragment {
   MapView mapView;
-
+  double distance = 0;
+  // 前回の更新時の位置情報
+  Location lastLocation = null;
+  @SuppressLint("MissingInflatedId")
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     //レイアウトファイルを取得
@@ -33,18 +39,17 @@ public class walkFragment extends Fragment {
     mapView = (MapView) view.findViewById(R.id.map);
     mapView.onCreate(savedInstanceState);
     mapView.getMapAsync(googleMap -> {
-      LocationManager locationManager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
-      if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-              ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+      LocationManager locationManager = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
+      if (ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+              ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
       // Permissionが必要です。
         return;
       }
-
       // 緯度と経度を取得
       Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
       double latitude = location.getLatitude();
       double longitude = location.getLongitude();
-      if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+      if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
         // TODO: Consider calling
         //    ActivityCompat#requestPermissions
         // here to request the missing permissions, and then overriding
@@ -58,6 +63,40 @@ public class walkFragment extends Fragment {
       googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 16.0f));
       Log.i("DEBUG", "onMapReady");
     });
+    LocationManager locationManager = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
+    final LocationListener[] locationListener = {null};
+    // clickイベントリスナーを登録(移動距離記録開始)
+    view.findViewById(R.id.start).setOnClickListener(v -> {
+      lastLocation = null;
+      // 移動距離の記録を開始するコード
+      locationListener[0] = new LocationListener() {
+        @SuppressLint({"SetTextI18n", "DefaultLocale"})
+        @Override
+        public void onLocationChanged(Location location) {
+          // 前回の更新時の位置情報が存在する場合
+          if (lastLocation != null) {
+            // 移動距離を計算
+            distance += location.distanceTo(lastLocation);
+          }
+          // 位置情報を更新
+          lastLocation = location;
+          // 移動距離を表示するためのViewを取得
+          TextView distanceView = (TextView) view.findViewById(R.id.distanceView);
+          // 移動距離を表示
+          distanceView.setText("移動距離: " + String.format("%.1f", distance) + "m");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) { }
+        @Override
+        public void onProviderEnabled(String provider) { }
+        @Override
+        public void onProviderDisabled(String provider) { }
+      };
+      locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener[0]);
+    });
+    // clickイベントリスナーを登録(移動距離記録終了)
+    view.findViewById(R.id.stop).setOnClickListener(v -> locationManager.removeUpdates(locationListener[0]));
     return view;
   }
 
